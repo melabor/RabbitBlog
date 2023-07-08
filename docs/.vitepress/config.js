ed35761fb90cc5bb2d70c8f374bd3e7124933dab
wanggaoxian <>
2023-07-08 20:17:09 +0800

    init

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,74 @@
+module.exports = {
+  title: '兔子前端',
+  description: '欢迎来到兔子前端',
+  themeConfig: {
+    lastUpdatedText: '',
+    nav: [
+      {
+        text: '免费路线',
+        items: [
+          { text: 'HTML', link: '' },
+          { text: 'CSS', link: '' },
+          { text: 'JavaScript', link: '' },
+          { text: 'Typescript', link: '' },
+        ],
+      },
+      {
+        text: '前端',
+        items: [
+          { text: 'Vue', link: '' },
+          { text: 'React', link: '' },
+          { text: '微信小程序', link: '' },
+          { text: 'UniApp', link: '' },
+          { text: 'Taro', link: '' },
+        ],
+      },
+      {
+        text: '前端工具库',
+        items: [
+          { text: 'dayJs', link: '' },
+          { text: 'bigJs', link: '' },
+          { text: 'bigJs', link: '' },
+          { text: 'bigJs', link: '' },
+        ],
+      },
+      {
+        text: 'Nodejs',
+        items: [
+          { text: 'Chinese', link: '' },
+          { text: 'Japanese', link: '' },
+        ],
+      },
+      {
+        text: 'webGL',
+        items: [
+          { text: 'Chinese', link: '' },
+          { text: 'Japanese', link: '' },
+        ],
+      },
+      {
+        text: 'npm',
+        items: [
+          { text: 'Chinese', link: '' },
+          { text: 'Japanese', link: '' },
+        ],
+      },
+      {
+        text: '面试题',
+        items: [
+          { text: 'Chinese', link: '' },
+          { text: 'Japanese', link: '' },
+        ],
+      },
+      {
+        text: '关于',
+        items: [
+          { text: 'Chinese', link: '' },
+          { text: 'Japanese', link: '' },
+        ],
+      },
+    ],
+    socialLinks: [{ icon: 'github', link: 'https://github.com' }],
+  },
+  docFooter: { prev: '上一篇', next: '下一篇' },
+}
