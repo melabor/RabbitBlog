2731775db4be80a3f103dcc9d2b8825d1ff3aeb2
wanggaoxian <>
2023-07-09 05:14:20 +0800

    feat:测试

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -6,21 +6,17 @@ module.exports = {
     nav: [
       {
         text: '免费路线',
-        items: [
-          { text: 'HTML', link: '' },
-          { text: 'CSS', link: '' },
-          { text: 'JavaScript', link: '' },
-          { text: 'Typescript', link: '' },
-        ],
+        link: '/free/freeIndex.md',
+        activeMatch: '',
       },
       {
         text: '前端',
         items: [
-          { text: 'Vue', link: '' },
-          { text: 'React', link: '' },
-          { text: '微信小程序', link: '' },
-          { text: 'UniApp', link: '' },
-          { text: 'Taro', link: '' },
+          { text: 'Vue', link: '', activeMatch: '' },
+          { text: 'React', link: '', activeMatch: '' },
+          { text: '微信小程序', link: '', activeMatch: '' },
+          { text: 'UniApp', link: '', activeMatch: '' },
+          { text: 'Taro', link: '', activeMatch: '' },
         ],
       },
       {
@@ -68,7 +64,34 @@ module.exports = {
         ],
       },
     ],
-    socialLinks: [{ icon: 'github', link: 'https://github.com' }],
+    sidebar: [
+      {
+        text: 'Vue',
+        items: [{ text: '1' }, { text: '2' }, { text: '3' }, { text: '4' }],
+        collapsible: 'true',
+      },
+      {
+        text: 'React',
+        items: [{ text: '' }],
+      },
+      {
+        text: '小程序',
+        items: [{ text: '' }],
+      },
+      {
+        text: 'UniApp',
+        items: [{ text: '' }],
+      },
+      {
+        text: 'Taro',
+        items: [{ text: '' }],
+      },
+    ],
+    socialLinks: [{ icon: 'github', link: 'https://github.com/RabbitCodeHole' }],
+    footer: {
+      message: 'Released under the MIT License.',
+      copyright: 'Copyright © 2019-present Evan You',
+    },
   },
   docFooter: { prev: '上一篇', next: '下一篇' },
 }
